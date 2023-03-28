eb7b1ee6303839d4c58e3742f95145b91a8c3ac7
naveen-gokarapu <naveen.gokarapu@fictiv.com>
2023-03-28 12:23:02 +0530

    Login Details

--- poc/src/App.js
+++ poc/src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
+import React from "react";
+import Login from "./components/Login/Login";
+
+import "./components/styles.css";
 
 function App() {
   return (
-    <div>
-      <p>Learn React</p>
+    <div className="App">
+      {/* <header className="App-header">Login Form</header> */}
+      <div id="content">
+        <Login />
+      </div>
     </div>
   );
 }
