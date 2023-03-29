00c15c2d92e45ba0e98d7b371a51d4b0806147c9
naveen-gokarapu <naveen.gokarapu@fictiv.com>
2023-03-29 11:43:28 +0530

    Login Todo changes

--- poc/src/components/Todo/Todo.js
+++ poc/src/components/Todo/Todo.js
@@ -4,68 +4,78 @@ import "./Todo.css";
 const Todo = () => {
   const [input, setInput] = useState();
   const [items, setItems] = useState([]);
-  const [checked, setChecked] = useState(false);
+  // const [checked, setChecked] = useState(false);
 
   const addHandler = (e) => {
     e.preventDefault();
     if (input) {
-      setItems([...items, input]);
+      setItems([...items, { name: input, ischecked: false }]);
       setInput("");
     }
   };
 
   const deleteHandler = (ele) => {
+    console.log(ele);
     setItems(items.filter((x) => x !== ele));
   };
-
-  const cbHandler = (e) => {
-    // setItems(items.filter((x) => x !== ele));
-    // console.log(e);
-    // var temp = checked;
-    // if (e.target.checked) {
-    //   console.log(temp, e.target.checked);
-    //   temp = temp.filter((a) => {
-    //     console.log(a);
-    //     return (a = e.target.value);
-    //   });
-    // }
-    // e.target.checked
-    //   ? setChecked([...checked, e.target.value])
-    //   : setChecked([...temp]);
+  const cbHandler = (position) => {
+    items[position].ischecked = !items[position].ischecked;
+    setItems([...items]);
   };
-
   return (
     <div className="todo-form">
-      <form onSubmit={addHandler}>
+      <form onSubmit={addHandler} style={{ marginBottom: "15px" }}>
         <div className="title">
           <label> TODO LIST </label>{" "}
         </div>
-
-        <input
-          style={{ margin: "auto" }}
-          type="text"
-          id="inputTodo"
-          name="inputTodo"
-          placeholder="type a task"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-        />
-        <button type="submit">Add</button>
+        <div>
+          <input
+            style={{ width: "75%", float: "left", padding: "10px 5px" }}
+            type="text"
+            id="inputTodo"
+            name="inputTodo"
+            placeholder="Please add your task"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <button
+            type="submit"
+            style={{ width: "20%", float: "right", padding: "10px 5px" }}
+          >
+            Add
+          </button>
+        </div>
+        <div style={{ clear: "both" }}></div>
       </form>
 
       <div>
         {items.map((ele, i) => {
-          console.log(checked);
           return (
-            <div key={i}>
-              <input type="checkbox" value={checked} onClick={cbHandler} />
+            <div
+              key={i}
+              style={{
+                border: "1px solid #454545",
+                margin: "2px",
+                padding: "5px",
+                borderRadius: "5px",
+              }}
+            >
+              <input type="checkbox" onChange={() => cbHandler(i)} />
 
-              {checked ? (
-                <label style={{ textDecoration: "line-through" }}> {ele}</label>
+              {ele.ischecked ? (
+                <label style={{ textDecoration: "line-through" }}>
+                  {" "}
+                  {ele.name}
+                </label>
               ) : (
-                <label> {ele}</label>
+                <label> {ele.name}</label>
               )}
-              <button onClick={() => deleteHandler(ele)}>Delete</button>
+              <button
+                onClick={() => deleteHandler(ele)}
+                style={{ float: "right", padding: "7px", margin: "-6px" }}
+              >
+                Delete
+              </button>
             </div>
           );
         })}
